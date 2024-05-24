aed0233d1dd5b172efc76adfd34e69126b8cbe8f
Harry MacCode <emarh.harry.code@gmail.com>
2024-05-24 19:43:16 +0200

    Refactor

--- app/tours/page.tsx
+++ app/tours/page.tsx
@@ -8,10 +8,19 @@ type Tour = {
     price: string;
 };
 
-async function ToursPage() {
+const fecthTours = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     const response = await fetch(url);
     const data: Tour[] = await response.json();
-    console.log(data);
+
+    return data;
+};
+
+async function ToursPage() {
+    // const response = await fetch(url);
+    // const data: Tour[] = await response.json();
+    // console.log(data);
+    const data = await fecthTours();
 
     return (
         <section>
