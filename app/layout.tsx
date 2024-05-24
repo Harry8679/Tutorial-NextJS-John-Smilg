3bf01e76265727817b883a150709789f02bf32f9
Harry MacCode <emarh.harry.code@gmail.com>
2024-05-24 11:57:03 +0200

    Fonts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import './globals.css';
 import Navbar from '@/components/Navbar';
+import { Inter, Inconsolata } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
+// const inconsolata = Inconsolata({ subsets:['latin']});
 
 export default function RootLayout({ children } : { children: React.ReactNode }) {
   return <html lang='en'>
-    <body>
+    <body className={inter.className}>
       <Navbar />
       <main className='max-w-3xl mx-auto py-10'>{children}</main>
     </body>
