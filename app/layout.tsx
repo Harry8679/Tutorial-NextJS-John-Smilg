c4e228042a561faf99142e6bd39575a6a31f1858
Harry MacCode <emarh.harry.code@gmail.com>
2024-05-24 11:36:55 +0200

    Navbar Components

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import './globals.css';
+import Navbar from '@/components/Navbar';
 
 export default function RootLayout({ children } : { children: React.ReactNode }) {
   return <html lang='en'>
     <body>
-      <nav>Hello there</nav>
-      {children}
+      <Navbar />
+      <main className='max-w-3xl mx-auto py-10'>{children}</main>
     </body>
   </html>
 }
