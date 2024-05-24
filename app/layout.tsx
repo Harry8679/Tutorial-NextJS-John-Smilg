c1defe9ba47adda793e04476ef1f11ec3e09f29a
Harry MacCode <emarh.harry.code@gmail.com>
2024-05-24 12:13:48 +0200

    Metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import './globals.css';
 import Navbar from '@/components/Navbar';
+import type { Metadata } from 'next';
 import { Inter, Inconsolata } from 'next/font/google';
 
 const inter = Inter({ subsets: ['latin'] });
 // const inconsolata = Inconsolata({ subsets:['latin']});
+
+export const metada: Metadata = {
+  title: 'Next.js Project',
+  description: 'A Next.js project with TypeScript and TailwindCSS',
+  keywords: 'Next.js TypeScript, TailwindCSS',
+};
 
 export default function RootLayout({ children } : { children: React.ReactNode }) {
   return <html lang='en'>
